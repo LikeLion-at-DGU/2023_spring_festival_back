87f151b63d0b09bc014982751f70e0b26c1c0535
jenny <88226911+kmjenny@users.noreply.github.com>
2023-05-16 00:13:16 +0900

    feature/2: create NotificationAPI

--- notification/views.py
+++ notification/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import mixins
+from .models import *
+from .serializers import *
+from rest_framework import viewsets
 
-# Create your views here.
+class NotificationViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
+    serializer_class = NotificationSerializer
+    queryset = Notification.objects.all()
